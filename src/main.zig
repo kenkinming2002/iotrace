90a34d1cc7c4d2db85beb16935c0949fd98eda1e
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-11-17 03:33:09 +0800

    Report command that simply tally up all the statistic

--- src/main.zig
+++ src/main.zig
@@ -69,7 +69,26 @@ fn record(log_file_path: []const u8, command: [][*]u8) !void {
 }
 
 fn report(log_file_path: []const u8) !void {
-    _ = log_file_path;
+    var log_file = try std.fs.cwd().openFile(log_file_path, .{});
+    defer log_file.close();
+
+    var bytes_read: usize = 0;
+    var bytes_written: usize = 0;
+
+    var packet: Packet = undefined;
+    while (try log_file.readAll(std.mem.asBytes(&packet)) == @sizeOf(@TypeOf(packet))) {
+        if (packet.fd != 1)
+            continue;
+
+        switch (packet.type) {
+            .Read => bytes_read += packet.count,
+            .Write => bytes_written += packet.count,
+        }
+    }
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Bytes read: {d}\n", .{bytes_read});
+    try stdout.print("Bytes written: {d}\n", .{bytes_written});
 }
 
 fn usage(program_name: [*c]const u8) void {
