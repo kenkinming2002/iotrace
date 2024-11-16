2626b85d60ad644c3c549ea72d7745e541951dd7
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-11-17 03:25:15 +0800

    Subcommand

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,6 @@ const c = @import("c.zig");
 const sys = @import("sys.zig");
 const ptrace = @import("ptrace.zig");
 
-fn usage(program_name: [*c]const u8) void {
-    std.debug.print("Usage: {s} <output-file> <command>\n", .{program_name});
-}
-
 const PacketType = enum(u8) {
     Read,
     Write,
@@ -43,40 +39,78 @@ const Packet = extern struct {
 
 const Context = struct {
     timer: std.time.Timer,
-    output_file: std.fs.File,
+    log_file: std.fs.File,
 
-    fn init(output_file_path: []const u8) !Context {
+    fn init(log_file_path: []const u8) !Context {
         const timer = try std.time.Timer.start();
-        const output_file = try std.fs.cwd().createFile(output_file_path, .{});
-        return .{ .timer = timer, .output_file = output_file };
+        const log_file = try std.fs.cwd().createFile(log_file_path, .{});
+        return .{ .timer = timer, .log_file = log_file };
     }
 
     fn deinit(self: *Context) void {
-        self.output_file.close();
+        self.log_file.close();
     }
 
     fn callback(self: *Context, pid: c.pid_t, nr: usize, args: [6]usize, rval: isize, is_error: u8) anyerror!void {
         var packet = Packet.parse(pid, nr, args, rval, is_error) orelse return;
         packet.timestamp = self.timer.read();
-        try self.output_file.writeAll(std.mem.asBytes(&packet));
+        try self.log_file.writeAll(std.mem.asBytes(&packet));
     }
 };
 
-pub fn main() !void {
+fn record(log_file_path: []const u8, command: [][*]u8) !void {
     const allocator = std.heap.c_allocator;
 
-    const argv = std.os.argv;
-    const program_name = argv[0];
-    if (argv.len < 3) {
-        return usage(program_name);
-    }
-
-    const output_file_path = argv[1];
-    const command = argv[2..];
-
-    var context = try Context.init(std.mem.span(output_file_path));
+    var context = try Context.init(log_file_path);
     defer context.deinit();
 
     _ = try ptrace.spawnvp(command[0], command.ptr);
     try ptrace.trace_syscalls(allocator, &context, Context.callback);
+}
+
+fn report(log_file_path: []const u8) !void {
+    _ = log_file_path;
+}
+
+fn usage(program_name: [*c]const u8) void {
+    std.debug.print("Usage: {s} record|report ...\n", .{program_name});
+    std.debug.print("       {s} record <file> <command>\n", .{program_name});
+    std.debug.print("       {s} report <file>\n", .{program_name});
+}
+
+pub fn main() !void {
+    const argv = std.os.argv;
+    const program_name = argv[0];
+    if (argv.len < 2) {
+        return usage(program_name);
+    }
+
+    const subcommand = std.mem.span(argv[1]);
+    if (std.mem.eql(u8, subcommand, "record")) {
+        if (argv.len < 3) {
+            std.debug.print("Error: Missing argument <file>\n", .{});
+            return usage(program_name);
+        }
+
+        if (argv.len < 4) {
+            std.debug.print("Error: Missing argument <command>\n", .{});
+            return usage(program_name);
+        }
+
+        const log_file_path = std.mem.span(argv[2]);
+        const command = argv[3..];
+        return record(log_file_path, command);
+    }
+
+    if (std.mem.eql(u8, subcommand, "report")) {
+        if (argv.len != 3) {
+            std.debug.print("Error: Missing argument <file>\n", .{});
+            return usage(program_name);
+        }
+
+        const log_file_path = std.mem.span(argv[2]);
+        return report(log_file_path);
+    }
+
+    return usage(program_name);
 }
